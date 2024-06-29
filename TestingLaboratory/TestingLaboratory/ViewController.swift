25a9fa8d158c75c9dbc149e78193dd31bacfb8bf
HELLOHIDI <fbgmlwo123@naver.com>
2024-06-29 14:41:57 +0900

    [Setting] 프로젝트 세팅

---
+++ TestingLaboratory/TestingLaboratory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingLaboratory
+//
+//  Created by 류희재 on 6/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
